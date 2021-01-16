bb4e7ee8b5116d57e20bb8d1da41924ccec3ae9c
k1m0j <kimoji@posteo.de>
2021-01-16 12:04:52 +0100

    add locales

---
+++ whoneeds-app/config/i18n.js
@@ -0,0 +1,8 @@
+import de from '../locales/de.json'
+import cn from '../locales/cn.json'
+
+export default {
+  local: 'de',
+  fallbackLocale: 'de',
+  messages: {de, cn}
+}
--- whoneeds-app/nuxt.config.js
+++ whoneeds-app/nuxt.config.js
@@ -5,29 +5,41 @@ export default {
   head: {
     title: 'whoneeds-app',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    ['@nuxtjs/vuetify', vuetifyConfig]
+    ['@nuxtjs/vuetify', vuetifyConfig],
+    ['nuxt-i18n',
+      {
+        defaultLocale: 'de',
+        locales: [
+          {
+            code: 'de',
+            name: 'Deutsch'
+          },
+          {
+            code: 'cn',
+            name: '中文'
+          }
+        ]
+      }]
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -36,6 +48,5 @@ export default {
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {}
 }
