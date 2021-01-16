d340d073c058347a31925b9b7ebbf04e2a109e52
k1m0j <kimoji@posteo.de>
2021-01-16 12:46:40 +0100

    add LanguageSelector compo

---
+++ whoneeds-app/components/LanguageSelector.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="lang-dropdown">
+    <select v-model="$i18n.locale">
+      <option v-key="lang in $i18n.locale"
+              :key="lang.code"
+              :value="lang.code">{{ lang.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LanguageSelector"
+}
+</script>
+
+<style scoped>
+
+</style>
