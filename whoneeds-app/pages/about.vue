c214be6b7d7ad1a1c527582d133f5e538fa5a4eb
pomcom <pomcom@mailbox.org>
2021-01-18 19:17:06 +0100

    implement basic about page

--- whoneeds-app/pages/about.vue
+++ whoneeds-app/pages/about.vue
@@ -1,3 +1,66 @@
 <template>
-  <h2>Hi</h2>
+  <v-parallax
+    height="300"
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+  >
+  <v-row
+        v-if="title"
+        align="center"
+        class="ma-0 fill-height text-center"
+        justify="center"
+      >
+        <v-col cols="12">
+          <Heading
+            :title="title"
+            space="2"
+            weight="500"
+          />
+          <Divider
+            color="primary"
+            dense
+          />
+          <v-breadcrumbs
+            :items="items"
+            class="justify-center pa-0"
+            divider=">"
+          />
+        </v-col>
+      </v-row>
+
+</v-parallax>
 </template>
+
+
+<script>
+
+  export default {
+    head() {
+      return {
+        title: 'About our Idea',
+      }
+    },
+    name: 'SectionHeroAlt',
+
+    provide: {
+      heading: { align: 'center' },
+    },
+
+    data: () => ({
+      title: 'ABOUT',
+
+    }),
+
+    computed: {
+      gradient () {
+
+      },
+
+      items () {
+        return [
+          { text: 'HOME', to: '/' },
+          { text: this.title },
+        ]
+      },
+    },
+  }
+</script>
