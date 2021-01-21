a599aad21a633dada4e63580e046a90e64043181
pomcom <pomcom@mailbox.org>
2021-01-21 20:29:38 +0100

    add ResetController

--- whoneeds-api/src/main/kotlin/net/whoneeds/whoneedsapi/ApplicationConstants.kt
+++ whoneeds-api/src/main/kotlin/net/whoneeds/whoneedsapi/ApplicationConstants.kt
@@ -4,6 +4,7 @@ object RoutingEndpointConstants {
     const val LOGIN_ROUTE = "/login"
     const val LOGOUT_ROUTE = "/logout"
     const val USERS_ROUTE = "/users"
+    const val RESET_PASSWORD_ROUTE = "/resetPassword"
 }
 
 object SecurityConstants {
--- whoneeds-api/src/main/kotlin/net/whoneeds/whoneedsapi/adapters/api/pwReset/ResetController.kt
+++ whoneeds-api/src/main/kotlin/net/whoneeds/whoneedsapi/adapters/api/pwReset/ResetController.kt
@@ -1,6 +1,29 @@
 package net.whoneeds.whoneedsapi.adapters.api.pwReset
 
+import net.whoneeds.whoneedsapi.RoutingEndpointConstants.RESET_PASSWORD_ROUTE
+import net.whoneeds.whoneedsapi.adapters.api.user.UserAccountRepository
+import net.whoneeds.whoneedsapi.domain.model.UserAccount
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+
 /**
  * @author Lukas Schuetz <pomcom> 2021
- */class ResetController {
+ */
+@RestController
+@RequestMapping(RESET_PASSWORD_ROUTE)
+class ResetController(
+        private val userAccountRepository: UserAccountRepository
+) {
+
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun resetPassword(@RequestBody string: String) {
+        println(string)
+
+    }
+
+
 }
