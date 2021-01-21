1f5c572f306cb1a0ca4415a4ec2224dd14c25381
pomcom <pomcom@mailbox.org>
2021-01-21 11:10:04 +0100

    fix password forget form

---
+++ whoneeds-app/pages/changePassword.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="d-flex flex-column mx-auto col-8 col-md-5 py-12">
+        <v-card class="pa-6" elevation="6">
+          <v-card-title>
+            Hey
+          </v-card-title>
+          <v-card-subtitle>
+            Neues Password einrichten
+          </v-card-subtitle>
+          <validation-observer ref="observer" v-slot="{ invalid }">
+            <v-card-text class="pb-0">
+              <v-form>
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="E-Mail address"
+                  rules="required|email"
+                >
+                  <v-text-field
+                    ref="emailInput"
+                    v-model="email"
+                    :error-messages="errors"
+                    label="E-mail"
+                    required
+                    outlined
+                  />
+                </validation-provider>
+              </v-form>
+            </v-card-text>
+
+            <v-card-actions class="pt-0 px-4">
+              <v-btn white width="fit-content" :disabled="invalid" @click="submit">
+                <v-icon class="mr-3">
+                  {{ forgotIcon }}
+                </v-icon>Send link
+              </v-btn>
+            </v-card-actions>
+          </validation-observer>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
+
+export default {
+  auth: false,
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
+  data: () => ({
+    forgotIcon: 'mdi-email-send-outline',
+    email: ''
+  }),
+  mounted () {
+    this.focusEmailInput()
+  },
+  methods: {
+    submit () {
+      this.$refs.observer.validate()
+      this.transfer()
+      this.$toast
+        .success('A password reset link has been emailed to you')
+        .goAway(10000)
+      this.focusEmailInput()
+      this.clear()
+    },
+    focusEmailInput () {
+      this.$refs.emailInput.focus()
+    },
+    clear () {
+      this.email = ''
+      this.$refs.observer.reset()
+    },
+    transfer () {
+      this.$axios.post('/resetPassword', { mailTo: this.email })
+        .then(() => {
+          this.$toast.success('juhu')
+        })
+        .catch(() => {
+          this.$toast.success('manno')
+        })
+    }
+  }
+}
+</script>
