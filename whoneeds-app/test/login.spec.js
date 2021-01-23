f983fd39cd1bce1effade674ac161090dc656ab9
Joscha Seelig <s0566845@htw-berlin.de>
2021-01-23 16:13:07 +0100

    Add some first frontend tests

--- whoneeds-app/test/login.spec.js
+++ whoneeds-app/test/login.spec.js
@@ -1,8 +1,27 @@
 import { mount } from '@vue/test-utils'
 import Login from '../pages/login.vue'
 
-test('should render login form', () => {
+describe('Login', () => {
+  // Inspect the raw component options
+  it('has data', () => {
+    expect(typeof Login.data).toBe('function')
+  })
+})
+
+describe('Mounted Login page', () => {
   const wrapper = mount(Login, {})
 
-  expect(wrapper.text()).toContain('Sign In')
+  test('is instantiated', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  it('has a heading', () => {
+    expect(wrapper.html()).toContain('<h2>Sign In</h2>')
+  })
+
+  test('should output error message for non valid email', () => {
+    wrapper.setData()
+    // todo...
+    expect(wrapper).toBeTruthy()
+  })
 })
