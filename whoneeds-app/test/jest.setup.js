85708b8a4433c8d3470754f72d2b236623ed1cfa
Joscha Seelig <s0566845@htw-berlin.de>
2021-01-25 02:20:43 +0100

    FIX failing tests and setup to connect to backend

--- whoneeds-app/test/jest.setup.js
+++ whoneeds-app/test/jest.setup.js
@@ -1,5 +1,19 @@
 import '@testing-library/jest-dom'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import { extend } from 'vee-validate'
+import { required, email, max, min } from 'vee-validate/dist/rules.umd.js'
+
+extend('required', required)
+extend('email', email)
+extend('max', max)
+extend('min', min)
+extend('confirmedBy', {
+  params: ['target'],
+  validate (value, { target }) {
+    return value === target
+  },
+  message: '{_field_} does not match'
+})
 
 Vue.use(Vuetify)
