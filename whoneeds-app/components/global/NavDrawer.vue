919fa5040fa9702e67e6f2831ad2e18be49d615b
Joscha Seelig <s0566845@htw-berlin.de>
2021-01-25 07:56:07 +0100

    fix AppDrawer bug for logout button

--- whoneeds-app/components/global/NavDrawer.vue
+++ whoneeds-app/components/global/NavDrawer.vue
@@ -10,7 +10,13 @@
     v-on="$listeners"
   >
     <v-list color="#303f69" shaped>
-      <v-list-item v-for="item in items" :key="item.title" :to="item.path" color="green">
+      <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        :to="item.path"
+        color="green"
+        @click="item.logout ? logout() : () => {}"
+      >
         <v-list-item-icon>
           <v-icon v-text="item.icon" />
         </v-list-item-icon>
@@ -28,7 +34,14 @@ export default {
   props: {
     items: {
       type: Array,
-      default: () => ([])
+      default: () => []
+    }
+  },
+  methods: {
+    async logout () {
+      await this.$auth
+        .logout()
+        .then(() => this.$toast.success('You have been logged out, Ciao!'))
     }
   }
 }
--- whoneeds-app/plugins/auth.js
+++ whoneeds-app/plugins/auth.js
@@ -3,8 +3,10 @@ export default function ({ $auth, $toast }) {
   $auth.onError((error, name, endpoint) => {
     console.error(name, error)
     $toast.clear()
-    if (error.response && error.response.status === 401) {
-      $toast.error('Login credentials invalid!')
+    if (error.response &&
+      (error.response.status === 401 ||
+        error.response.status === 403)) {
+      $toast.error('You are unauthorized!')
     } else {
       $toast.error('Oops.. Something went wrong. Please try again later')
     }
