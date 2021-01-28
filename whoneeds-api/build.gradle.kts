b112cda7718393dccbc1453edde6d0ffec747bc8
pomcom <pomcom@mailbox.org>
2021-01-28 21:50:32 +0100

    adjust mail config for testing

--- whoneeds-api/build.gradle.kts
+++ whoneeds-api/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
     implementation("org.springframework.vault:spring-vault-core:2.3.0")
     implementation("org.springframework.boot:spring-boot-starter-validation:2.4.1")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
 
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
