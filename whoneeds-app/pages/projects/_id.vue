bca92ca115151c94f3122cbbf93f26539676ac0e
Joscha Seelig <s0566845@htw-berlin.de>
2021-03-26 16:46:26 +0100

    fix routing to project after creation

--- whoneeds-app/pages/projects/_id.vue
+++ whoneeds-app/pages/projects/_id.vue
@@ -1,27 +1,47 @@
 <template>
-  <div>
-    <div v-if="!$fetchState.pending">
-      <h2>{{ project.name }}</h2>
-      <h4>Description</h4>
-      <p>{{ project.description }}</p>
-      <div class="category-chips">
-        <v-chip
-          v-for="category in project.categories"
-          :key="category"
-          :color="getChipColor()"
-          small
-        >
-          {{ category }}
-        </v-chip>
+  <v-container fill-height>
+    <v-row justify="center" align="center">
+      <div v-if="!$fetchState.pending">
+        <h2>{{ project.name }}</h2>
+        <v-divider />
+        <div class="category-chips">
+          <v-chip
+            v-for="category in project.categories"
+            :key="category"
+            :color="getChipColor()"
+            small
+          >
+            {{ category }}
+          </v-chip>
+        </div>
+        <h4>Description</h4>
+        <p>{{ project.description }}</p>
+        <div v-if="project.members.length > 0">
+          <h4>Members</h4>
+          <ul>
+            <li v-for="member in project.members" :key="member.id">
+              {{ member.name }} ({{ member.email }})
+            </li>
+          </ul>
+        </div>
+        <p v-else>
+          The project has no additional members yet.
+        </p>
+
+        <h4>Address</h4>
+        <p>Name: {{ project.address.name }}</p>
+        <p>Street: {{ project.address.street }} {{ project.address.nr }}</p>
+        <p>Zip: {{ project.address.zip }}</p>
+        <p>City: {{ project.address.city }}</p>
+        <p>State: {{ project.address.state }}</p>
       </div>
-      <h4>Address</h4>
-      <p>Name: {{ project.address.name }}</p>
-      <p>Street: {{ project.address.street }} {{ project.address.nr }}</p>
-      <p>Zip: {{ project.address.zip }}</p>
-      <p>City: {{ project.address.city }}</p>
-      <p>State: {{ project.address.state }}</p>
-    </div>
-  </div>
+    </v-row>
+    <v-row>
+      <v-btn @click="$router.push('/projects/2')">
+        Click Me
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -49,3 +69,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.category-chips {
+  margin-right: 10px;
+  margin-top: 5px;
+}
+</style>
