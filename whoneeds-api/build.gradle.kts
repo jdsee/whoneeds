47cdda0984e6ad46e165940d2cd7141b8a3beac2
Joscha Seelig <s0566845@htw-berlin.de>
2021-03-26 20:18:24 +0100

    connect to external database

--- whoneeds-api/build.gradle.kts
+++ whoneeds-api/build.gradle.kts
@@ -41,7 +41,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
