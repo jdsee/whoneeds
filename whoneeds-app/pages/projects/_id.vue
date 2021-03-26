cb9aaa4ec0fafa53b2d1d190a78701cdae6076f0
Joscha Seelig <s0566845@htw-berlin.de>
2021-03-27 00:30:48 +0100

    add project list to profile page

--- whoneeds-app/pages/projects/_id.vue
+++ whoneeds-app/pages/projects/_id.vue
@@ -1,23 +1,29 @@
 <template>
-  <v-container fill-height>
+  <v-container fill-height class="content">
+    <v-overlay :value="$fetchState.pending || project == null">
+      <v-progress-circular indeterminate size="64" />
+    </v-overlay>
+
     <v-row justify="center" align="center">
-      <div v-if="!$fetchState.pending && project !== undefined">
-        <h2>{{ project.name }}</h2>
-        <v-divider />
-        <div class="category-chips">
+      <div v-if="project !== null">
+        <v-col class="boxed-heading">
+          <h1>{{ project.name }}</h1>
+        </v-col>
+        <v-col>
           <v-chip
             v-for="(category, index) in project.categories"
             :key="category"
+            class="single-chip"
             :color="getChipColor(index)"
             small
           >
             {{ category }}
           </v-chip>
-        </div>
+        </v-col>
         <h4>Description</h4>
         <p>{{ project.description }}</p>
+        <h4>Members</h4>
         <div v-if="project.members.length > 0">
-          <h4>Members</h4>
           <ul>
             <li v-for="member in project.members" :key="member.id">
               {{ member.name }} ({{ member.email }})
@@ -25,15 +31,20 @@
           </ul>
         </div>
         <p v-else>
-          The project has no additional members yet.
+          This project has no additional members yet.
         </p>
 
         <h4>Address</h4>
-        <p>Name: {{ project.address.name }}</p>
-        <p>Street: {{ project.address.street }} {{ project.address.nr }}</p>
-        <p>Zip: {{ project.address.zip }}</p>
-        <p>City: {{ project.address.city }}</p>
-        <p>State: {{ project.address.state }}</p>
+        Name: {{ project.address.name }}
+        <br>
+        Street: {{ project.address.street }} {{ project.address.nr }}
+        <br>
+        Zip: {{ project.address.zip }}
+        <br>
+        City: {{ project.address.city }}
+        <br>
+        State: {{ project.address.state }}
+        <br>
       </div>
       <div v-else>
         <h2>This project does not exist.</h2>
@@ -43,10 +54,10 @@
         </v-btn>
       </div>
     </v-row>
-    <v-row v-if="project !== undefined">
+    <v-row v-if="project !== null" class="btn-delete">
       <v-dialog v-model="deleteDialog" max-width="290">
         <template #activator="{ on, attrs }">
-          <v-btn color="error" text dark v-bind="attrs" v-on="on">
+          <v-btn color="error" outlined v-bind="attrs" v-on="on">
             Delete Project
           </v-btn>
         </template>
@@ -55,15 +66,15 @@
             Are you sure you want to delete this project?
           </v-card-title>
           <v-card-text>
-            If you submit now the project "{{ project.name }}" will be
-            permanently deleted.
+            If you submit now the project
+            <e>"{{ project.name }}"</e> will be permanently deleted.
           </v-card-text>
           <v-card-actions>
             <v-spacer />
             <v-btn color="secondary" text @click="deleteDialog = false">
               Cancel
             </v-btn>
-            <v-btn color="error" text @click="deleteProject">
+            <v-btn color="error" outlined @click="deleteProject">
               Delete Project
             </v-btn>
           </v-card-actions>
@@ -76,7 +87,7 @@
 <script>
 export default {
   data: () => ({
-    project: undefined,
+    project: null,
     chipColors: ['green', 'blue', 'purple', 'red', 'orange', 'yellow'],
     deleteDialog: false
   }),
@@ -118,8 +129,35 @@ export default {
 </script>
 
 <style scoped>
-.category-chips {
-  margin-right: 10px;
-  margin-top: 5px;
+e {
+  font-style: italic;
+  color: teal;
+}
+.content {
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.theme--light .boxed-heading {
+  text-align: center;
+  box-shadow: 0px 2px 4px -3px teal;
+  border-radius: 10px;
+  border: 2px solid teal;
+}
+.theme--dark .boxed-heading {
+  text-align: center;
+  box-shadow: 0px 2px 4px -3px lightgrey;
+  border-radius: 10px;
+  border: 2px solid teal;
+}
+.chip-group {
+  width: 50%;
+}
+.single-chip {
+  margin-right: 4px;
+}
+.btn-delete {
+  margin-top: 30px;
+  margin-bottom: 10px;
 }
 </style>
--- whoneeds-app/pages/users/me.vue
+++ whoneeds-app/pages/users/me.vue
@@ -1,22 +1,61 @@
 <template>
   <v-container fill-height>
     <v-row justify="center" align="center">
-      <div class="content">
-        <h1>My Profile</h1>
-        <hr>
-        <p>
-          <strong>Name:</strong>
-          {{ `${user.name} ${user.surname}` }}
-        </p>
-        <p>
-          <strong>E-Mail:</strong>
-          {{ user.email }}
-        </p>
-      </div>
+      <v-col cols="12">
+        <div class="content">
+          <h1>My Profile</h1>
+          <v-divider />
+          <div class="user-details">
+            <p>
+              <strong>Name:</strong>
+              {{ `${user.name} ${user.surname}` }}
+              <br />
+              <strong>E-Mail:</strong>
+              {{ user.email }}
+            </p>
+          </div>
+        </div>
+      </v-col>
+      <v-col>
+        <v-list>
+          <v-list-group
+            :value="true"
+            prepend-icon="mdi-earth"
+            no-action
+            sub-group
+            color="teal"
+          >
+            <template #activator>
+              <v-list-item-title>Your Projects</v-list-item-title>
+            </template>
+
+            <v-list-item
+              v-for="project in user.projects"
+              :key="project.id"
+              :to="'/projects/' + project.id"
+            >
+              <v-list-item-title v-text="project.name" />
+              <div class="chip-group">
+                <v-chip
+                  v-for="(category, index) in project.categories"
+                  :key="category"
+                  class="single-chip"
+                  :color="getChipColor(index)"
+                  x-small
+                >
+                  {{ category }}
+                </v-chip>
+              </div>
+              <v-divider :key="project.id" />
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-col>
     </v-row>
+    <v-divider />
     <v-row>
       <v-col cols="12">
-        <v-btn color="primary" to="/projects/new">
+        <v-btn color="primary" to="/projects/new" class="btn-create">
           <v-icon left>
             mdi-plus
           </v-icon>
@@ -24,7 +63,12 @@
         </v-btn>
       </v-col>
       <v-col cols="12">
-        <v-btn to="/changePassword" class="ma-2" text color="secondary">
+        <v-btn
+          to="/changePassword"
+          class="ma-2 btn-reset"
+          text
+          color="secondary"
+        >
           <v-icon left color="primary">
             mdi-refresh
           </v-icon>
@@ -38,24 +82,45 @@
 <script>
 export default {
   data: () => ({
-    user: {
-      name: '',
-      surname: '',
-      email: ''
-    }
+    user: null,
+    chipColors: ["green", "blue", "purple", "red", "orange", "yellow"]
   }),
-  created () {
-    const authenticated = this.$auth.user
-    this.user.name = authenticated.name
-    this.user.surname = authenticated.surname
-    this.user.email = authenticated.email
+  async fetch() {
+    await this.$axios.get("/projects").then(request => {
+      this.user.projects = request.data;
+    });
+  },
+  created() {
+    this.user = this.$auth.user;
+  },
+  methods: {
+    getChipColor(i) {
+      return this.chipColors[i % 7];
+    }
   }
-}
+};
 </script>
 
 <style scoped>
 v-btn {
   margin-top: 5px;
   margin-bottom: 5px;
+}
+.user-details {
+  margin-top: 50 px;
+}
+.chip-group {
+  width: 100%;
+}
+.single-chip {
+  margin-right: 2px;
+}
+.btn-create {
+  margin-top: 40px;
+  margin-bottom: -20px;
+}
+.btn-reset {
+  margin-top: 5px;
+  margin-bottom: 40px;
 }
 </style>
